ed8f909da97a33359fe100b1bdb4ad4ba8fac709
Aki Kurvinen <aki.kurvinen@hotmail.fi>
2025-02-24 15:46:20 +0200

    add dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://akikurvinen.github.io',
-    base: 'astro-demo',
+  site: 'https://akikurvinen.github.io',
+  base: 'astro-demo',
+  integrations: [preact()],
 });
